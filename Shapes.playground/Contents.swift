da7bb0e07e1c24f0260b044c047adb1cf4905218
JonMercer <mercer.jon@gmail.com>
2016-04-07 20:44:34 -0700

    finished enums

--- Shapes.playground/Contents.swift
+++ Shapes.playground/Contents.swift
@@ -1,12 +1,19 @@
 import Foundation
 
-enum ColorName : String {
-    case Black, Silver, Gray, White, Maroon, Red, Purple, Fuchsia, Green, Lime, Olive, Yellow, Navy, Blue, Teal, Aqua
-}
 
+/* Why Enums
+ Enumerations are great for picking items from a list of well-known things, such as days of the week, faces of a coin or states in a state machine. 
+ */
 enum CSSColor {
     case Named(ColorName)
     case RGB(UInt8, UInt8, UInt8)
+}
+
+extension CSSColor {
+    //pushes colorname namespace into CSSColor
+    enum ColorName : String {
+        case Black, Silver, Gray, White, Maroon, Red, Purple, Fuchsia, Green, Lime, Olive, Yellow, Navy, Blue, Teal, Aqua
+    }
 }
 
 extension CSSColor : CustomStringConvertible {
@@ -24,6 +31,8 @@ let color1 = CSSColor.Named(.Red)
 let color2 = CSSColor.RGB(0xAA, 0xAA, 0xAA)
 print("color1 = \(color1), color2 = \(color2)") // prints color1 = Red, color2 = #AAAAAA
 
+
+//custom initializer method for gray
 extension CSSColor {
     init(gray: UInt8) {
         self = .RGB(gray, gray, gray)
@@ -32,3 +41,7 @@ extension CSSColor {
 
 let color3 = CSSColor(gray: 0xaa)
 print(color3)
+
+enum Math {
+    static let pi = 3.1415926535897932384626433832795028841971694
+}
